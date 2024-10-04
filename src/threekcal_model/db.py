229d0104113d03722f5d048c5e2dbefe39bd614d
GITSangWoo <dogcenta@gmail.com>
2024-10-04 19:17:10 +0900

    db name fixed

--- src/threekcal_model/db.py
+++ src/threekcal_model/db.py
@@ -6,6 +6,6 @@ def get_connection():
     connection = pymysql.connect(host=os.getenv("DB_IP",'localhost'),
                                  port =int(os.getenv("DB_PORT", "53306")),
                                  user = 'master', password = '1234',
-                                 database = 'db',
+                                 database = 'modeldb',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
