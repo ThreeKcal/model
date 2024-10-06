9d61ceb8a7a618ce4e1b3ffe2411863d5e2c2abb
hahahellooo <hahahello777@gmail.com>
2024-10-06 19:28:03 +0900

    add ORDER BY num

--- src/threekcal_model/worker.py
+++ src/threekcal_model/worker.py
@@ -11,11 +11,11 @@ def run():
     connection = get_connection()
     with connection:
         with connection.cursor() as cursor:
-            sql = "SELECT num, comments, request_user, request_time, prediction_result, prediction_score, prediction_time FROM comments WHERE prediction_result IS NULL LIMIT 1"
+            sql = "SELECT num, comments, request_user, request_time, prediction_result, prediction_score, prediction_time FROM comments WHERE prediction_result IS NULL ORDER BY num ASC LIMIT 1"
             cursor.execute(sql)
             result = cursor.fetchone()
     print(result)
-
+    print("*"*33)
     if result is None:
         data = {"message":f"❌예측할 모델이 없습니다❌"}
         print(data)
@@ -29,7 +29,7 @@ def run():
         prediction_time = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
         log_data=[num,prediction_result,prediction_score,prediction_time]
     print(log_data)
+    print("*"*33)
     return log_data
 
 
-run()
