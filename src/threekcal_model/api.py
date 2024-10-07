d129548b28cbe226fb197486acfbb97000ddac0c
sunwoo <summerham22@gmail.com>
2024-10-07 17:21:32 +0900

    remove dependencies

--- src/threekcal_model/api.py
+++ src/threekcal_model/api.py
@@ -1,8 +1,6 @@
 from typing import Union
 
 from fastapi import FastAPI
-from transformers import pipeline
-from threekcal_model.utils import get_label, get_score
 
 app = FastAPI()
 
@@ -14,11 +12,6 @@ def read_root():
 
 @app.get("/comments/")
 def read_item(user: str, comments: str):
-    #모델 불러오기
-    model = pipeline("text-classification", model="michellejieli/emotion_text_classifier")
-    
-    #모델 입히기
-    prediction = model(f"{comments}")
 
     #DB에 저장하기
     import pymysql.cursors
@@ -29,15 +22,10 @@ def read_item(user: str, comments: str):
     import jigeum.seoul
     from threekcal_model.db import dml
     insert_row = dml(sql, comments, jigeum.seoul.now(), user)
-    label = get_label(prediction)
-    score = get_score(prediction)
-
 
     #출력값
     return {
             "comments": comments,
             "request_time": jigeum.seoul.now(),
             "request_user": user, 
-            "label": label,
-            "score": score
             }
