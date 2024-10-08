5dd665c6398b8b52f5ba7def9457ad5865f18c5b
sunwoo <summerham22@gmail.com>
2024-10-08 11:17:31 +0900

    done page2

--- src/threekcal_model/streamlit/pages/2_코멘트_라벨적기.py
+++ src/threekcal_model/streamlit/pages/2_코멘트_라벨적기.py
@@ -12,3 +12,32 @@ def load_data():
 
 data = load_data()
 df = pd.DataFrame(data)
+st.write(df)
+
+num = st.text_input("num", "")
+remark = st.text_input("의견을 남겨주세요", "")
+label = st.text_input("정답", "")
+
+def input_label():
+    import pymysql.cursors
+    import os
+    from threekcal_model.db import get_conn
+    try:
+        if st.button("Submit"):
+            if remark and label and num:
+                st.write(f"{num}에 {remark}와 {label}이(가) 업데이트 되었습니다!")
+                conn = get_conn()
+                with conn.cursor() as cursor:
+                    sql = "UPDATE comments SET remark = %s, label = %s WHERE num = %s"
+                    cursor.execute(sql, (remark, label, num))
+
+                # 변경 사항 저장 (commit)
+                conn.commit()
+
+            else:
+                st.write("세 항목을 모두 입력해 주세요.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+input_label()
