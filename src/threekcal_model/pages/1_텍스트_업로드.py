5cef9f883dc89d54ce016cb81a00bc9c095c2ef3
GITSangWoo <dogcenta@gmail.com>
2024-10-09 19:39:48 +0900

    fix 1

--- src/threekcal_model/pages/1_텍스트_업로드.py
+++ src/threekcal_model/pages/1_텍스트_업로드.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import json
+import pandas as pd 
 
 user = st.text_input("유저ID", "")
 title = st.text_input("리뷰", "")
@@ -16,6 +17,11 @@ def load_data():
 
                 if response.status_code == 200:
                     st.write(f"유저ID [{user}]의 리뷰 \"{title}\"가 성공적으로 업로드 되었습니다!")
+                    url = 'http://54.180.132.11:8001/all'
+                    r = requests.get(url)
+                    d = r.json()
+                    df = pd.DataFrame(d)
+                    st.dataframe(df.tail(5))
                     st.write(f"모델에 따른 감정 예측은 이후 airflow를 통해 순차적으로 진행됩니다. 좌측의 \"코멘트 라벨적기\" 페이지를 통해 개별 결과 확인 및 피드백을, \"결과 통계\" 페이지를 통해 전체적인 모델 API의 성능 통계를 확인할 수 있습니다.")
                 else: 
                     st.write("Something went wrong!")
